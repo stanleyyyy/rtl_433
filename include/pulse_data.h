af4d81d331523c66eaeca5829927e515d2215289
Stanislav Ruzani <stanislav.ruzani@streamunlimited.com>
2024-12-03 15:12:07 +0100

    pulse_data: increased minimum gap to 15 ms to be able to receive data from large Sonoff buttons
    Signed-off-by: Stanislav Ruzani <stanislav.ruzani@streamunlimited.com>

--- include/pulse_data.h
+++ include/pulse_data.h
@@ -20,7 +20,7 @@
 #define PD_MAX_PULSES        1200 // Maximum number of pulses before forcing End Of Package
 #define PD_MIN_PULSES        16   // Minimum number of pulses before declaring a proper package
 #define PD_MIN_PULSE_SAMPLES 10   // Minimum number of samples in a pulse for proper detection
-#define PD_MIN_GAP_MS        10   // Minimum gap size in milliseconds to exceed to declare End Of Package
+#define PD_MIN_GAP_MS        15   // Minimum gap size in milliseconds to exceed to declare End Of Package
 #define PD_MAX_GAP_MS        100  // Maximum gap size in milliseconds to exceed to declare End Of Package
 #define PD_MAX_GAP_RATIO     10   // Ratio gap/pulse width to exceed to declare End Of Package (heuristic)
 #define PD_MAX_PULSE_MS      100  // Pulse width in ms to exceed to declare End Of Package (e.g. for non OOK packages)
